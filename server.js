df15c5c436d72a72bfac4c5e98bfc5d5811effbc
Josquin2 <Josquinwork@mail.ru>
2024-11-04 17:24:38 +0300

    fix: deleted get/:id request

--- server.js
+++ server.js
@@ -6,18 +6,11 @@ const { sql } = require("drizzle-orm");
 const usersTable = require("./contact");
 const cors = require("cors");
 dotenv.config();
-// Creating server
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Handling GET / request
-app.get("/", (req, res) => {
-  res.send("This is the express server");
-});
-
-// Handling GET /hello request
 app.get("/contacts", async (req, res) => {
   try {
     const users = await db.select().from(usersTable).execute();
@@ -27,33 +20,12 @@ app.get("/contacts", async (req, res) => {
   }
 });
 
-app.get("/contacts/:id", async (req, res) => {
-  try {
-    const user = await db
-      .select()
-      .from(usersTable)
-      .where((tbl) => tbl.id.equals(req.params.id))
-      .execute();
-
-    if (user.length) {
-      res.json(user[0]);
-    } else {
-      res.status(404).json({ error: "User not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: error });
-  }
-});
-
 app.post("/contacts", async (req, res) => {
   try {
     const { name, email, phone } = req.body;
     console.log(req.body);
 
-    const [id] = await db
-      .insert(usersTable)
-      .values({ name, email, phone })
-      .execute();
+    await db.insert(usersTable).values({ name, email, phone }).execute();
 
     res.status(200).json(await db.select().from(usersTable));
   } catch (err) {
@@ -115,7 +87,6 @@ app.delete("/contacts", async (req, res) => {
   }
 });
 
-// Server listening to port 3000
 app.listen(5101, () => {
   console.log("Server is Running");
 });
